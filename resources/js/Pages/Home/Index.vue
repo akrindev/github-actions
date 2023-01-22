4a17e7e5478b1234b420efbdcfbf771eb8b079ce
akrindev <akrinmin@gmail.com>
2023-01-22 14:17:09 +0700

    init laravel with inertiajs vue

---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,12 @@
+<script setup>
+const name = 'syakirin amin'
+
+defineProps({ say: Object })
+</script>
+
+<template>
+    <div>
+        Hello, ini awal dari sebuah percobaan inertiajs dengan vuejs. <br>
+        My name is {{ name }} and I want to say {{ say }}
+    </div>
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inertiajs with Vue</title>
+</head>
+@vite(['resources/js/app.js', 'resources/js/app.js'])
+@inertiaHead
+<body>
+    @inertia
+</body>
+</html>
